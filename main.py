ed8d142745a7d176e06b4572a429eec377d5329a
AFaraazK <92804285+AFaraazK@users.noreply.github.com>
2023-03-03 21:42:44 -0500

    pseudo pseudocode

---
+++ main.py
@@ -0,0 +1,16 @@
+# REMAKE THIS IN JS
+# This is just establishing the script idea
+# ADD: TKINTER
+
+# enter 4 names
+# pull initials, store in a list of tuples
+
+# find every arrangement of those letters
+# using loops? inefficient, but works for now
+
+# let user select which dictionary to use
+# search the dictionary file using these rules
+# letters must occur in the order they're shown
+# W <-> U, K -> C
+# maybe work backwards? 
+# >> find any word with all of those letters, then arrange the letter. 
